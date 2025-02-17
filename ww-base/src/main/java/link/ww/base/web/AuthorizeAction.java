90306b50634cd600698d1bb000b4d3c1281281cb
王晓磊 <383537156@qq.com>
2025-02-17 16:22:23 +0800

    feat: AuthorizeAction 中的方法支持post、get

--- ww-base/src/main/java/link/ww/base/web/AuthorizeAction.java
+++ ww-base/src/main/java/link/ww/base/web/AuthorizeAction.java
@@ -4,7 +4,6 @@ import link.web.annotation.WrapperWebResult;
 import link.ww.base.service.AuthorizeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,11 +26,11 @@ public class AuthorizeAction {
 
   /**
    * 获取企业微信网页授权链接
-   * 
+   *
    * @param redirectUrl 重定向地址，用户授权后将被重定向到此地址
    * @return 授权链接，用户通过此链接进行授权
    */
-  @GetMapping("#{wwBaseProperties.auth.requestAuthorizeUri}")
+  @RequestMapping("#{wwBaseProperties.auth.requestAuthorizeUri}")
   public String url(String redirectUrl) {
     String state = UUID.randomUUID().toString(); // 生成随机状态参数，用于防止CSRF攻击
     return authorizeService.getAuthorizeUrl(redirectUrl, state);
@@ -39,11 +38,11 @@ public class AuthorizeAction {
 
   /**
    * 获取企业微信网页授权链接（开放式授权）
-   * 
+   *
    * @param redirectUrl 重定向地址，用户授权后将被重定向到此地址
    * @return 授权链接，用户通过此链接进行授权以获取详细信息
    */
-  @GetMapping("#{wwBaseProperties.auth.requestWebAuthorizeUri}")
+  @RequestMapping("#{wwBaseProperties.auth.requestWebAuthorizeUri}")
   public String webUrl(String redirectUrl) {
     String state = UUID.randomUUID().toString(); // 生成随机状态参数，用于防止CSRF攻击
     return authorizeService.getWebAuthorizeUrl(redirectUrl, state);
